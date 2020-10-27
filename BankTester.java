3e886574fccd34eb202b1b2fc82067c1b835b253
Sarah Leong <sleong20@stuy.edu>
2020-10-27 17:19:05 -0400

    Tested withdraw method

--- BankTester.java
+++ BankTester.java
@@ -8,12 +8,22 @@ public class BankTester {
         // account1.setPassword("crabs");
         // System.out.println(account1.forgotPassword());
 
-        System.out.println(account1.deposit(-10));
+        // System.out.println(account1.deposit(-10));
+        // System.out.println(account1.getBalance());
+        // System.out.println(account1.deposit(10.23));
+        // System.out.println(account1.deposit(20.22343));
+        // System.out.println(account1.getBalance());
+        // System.out.println(account1.deposit(13));
+        // System.out.println(account1.getBalance());
+        
+        System.out.println(account1.deposit(100));
+        System.out.println(account1.withdraw(-10));
         System.out.println(account1.getBalance());
-        System.out.println(account1.deposit(10.23));
-        System.out.println(account1.deposit(20.22343));
+        System.out.println(account1.withdraw(10.50));
         System.out.println(account1.getBalance());
-        System.out.println(account1.deposit(13));
+        System.out.println(account1.withdraw(23424));
+        System.out.println(account1.getBalance());
+        System.out.println(account1.withdraw(23.234124312));
         System.out.println(account1.getBalance());
     }
 }
