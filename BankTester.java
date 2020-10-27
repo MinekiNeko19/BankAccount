4f0504d295d9327b36992c37c3047f4889645770
Sarah Leong <sleong20@stuy.edu>
2020-10-27 16:53:48 -0400

    Tested deposit method

--- BankTester.java
+++ BankTester.java
@@ -4,8 +4,16 @@ public class BankTester {
         // System.out.println(account1.getBalance());
         // System.out.println(account1.getAccount());
 
-        System.out.println(account1.forgotPassword());
-        account1.setPassword("crabs");
-        System.out.println(account1.forgotPassword());
+        // System.out.println(account1.forgotPassword());
+        // account1.setPassword("crabs");
+        // System.out.println(account1.forgotPassword());
+
+        System.out.println(account1.deposit(-10));
+        System.out.println(account1.getBalance());
+        System.out.println(account1.deposit(10.23));
+        System.out.println(account1.deposit(20.22343));
+        System.out.println(account1.getBalance());
+        System.out.println(account1.deposit(13));
+        System.out.println(account1.getBalance());
     }
 }
