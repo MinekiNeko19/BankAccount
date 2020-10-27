47630a4278586753d7cf3ee27e1df38674b9bc94
Sarah Leong <sleong20@stuy.edu>
2020-10-27 16:22:18 -0400

    Tested setPassword()

--- BankTester.java
+++ BankTester.java
@@ -1,7 +1,11 @@
 public class BankTester {
     public static void main(String[] args) {
         BankAccount account1 = new BankAccount(0, "hackMe");
-        System.out.println(account1.getBalance());
-        System.out.println(account1.getAccount());
+        // System.out.println(account1.getBalance());
+        // System.out.println(account1.getAccount());
+
+        System.out.println(account1.forgotPassword());
+        account1.setPassword("crabs");
+        System.out.println(account1.forgotPassword());
     }
 }
